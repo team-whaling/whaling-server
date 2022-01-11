0b332840e0f20ef0802366ed62994f56206dc8f8
Min A <min2010011@gmail.com>
2022-01-12 03:39:34 +0900

    [WHAL-50] Feat: User 모델의 email 필드 제거

--- account/models.py
+++ account/models.py
@@ -11,11 +11,10 @@ class TimeStampedModel(models.Model):
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, user_id, email, password=None, **extra_fields):
+    def create_user(self, user_id, password=None, **extra_fields):
         try:
             user = self.model(
                 user_id=user_id,
-                email=self.normalize_email(email),
                 **extra_fields
             )
             user.set_password(password)
@@ -24,18 +23,17 @@ class UserManager(BaseUserManager):
         except Exception as e:
             print(e)
 
-    def create_superuser(self, user_id, email, password=None, **extra_fields):
+    def create_superuser(self, user_id, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         if extra_fields.get('is_superuser') is not True or extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True && is_superuser=True')
-        return self.create_user(user_id, email, password, **extra_fields)
+        return self.create_user(user_id, password, **extra_fields)
 
 
 class User(AbstractBaseUser, TimeStampedModel, PermissionsMixin):
-    user_id = models.BigIntegerField(primary_key=True)  # 카카오 회원 번호로 설정
-    email = models.EmailField(unique=True)
-    nickname = models.CharField(max_length=20, unique=True)
+    user_id = models.BigIntegerField('회원번호', primary_key=True)
+    nickname = models.CharField('닉네임', max_length=20, unique=True)
     acc_percent = models.FloatField('적중률', default=0.0)
     point = models.IntegerField('고래밥', default=100)
     profile_img = models.URLField('프로필 url', null=True)
@@ -45,7 +43,10 @@ class User(AbstractBaseUser, TimeStampedModel, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_id', 'nickname']
+    USERNAME_FIELD = 'user_id'
+    REQUIRED_FIELDS = ['nickname']
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.nickname
