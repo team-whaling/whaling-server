bd09ca7ea592146995e0aee1079dda1ff1333bdc
Min A <min2010011@gmail.com>
2022-01-14 00:57:53 +0900

    [WHAL-50] Feat: 토큰 재발급 및 검증 뷰 추가

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from .views import *
 
@@ -7,10 +8,15 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # 로그인/회원가입
     path('auth', kakao_login),
 
+    # 토큰 재발급 및 검증
+    path('token/refresh', TokenRefreshView.as_view()),
+    path('token/verify', TokenVerifyView.as_view()),
+
     # 테스트용
-    path('kakao/login', KakaoLoginRedirectTestView.as_view()),
-    path('kakao/login/callback', KakaoLoginRequestTestView.as_view()),
+    path('', include(router.urls)),
+    path('auth/kakao', KakaoLoginRedirectTestView.as_view()),
+    path('auth/kakao/callback', KakaoLoginRequestTestView.as_view()),
 ]
