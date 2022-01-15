9771bd80f9e2e88b1ec97c70842b984693da619c
Min A <min2010011@gmail.com>
2022-01-15 16:46:00 +0900

    [WHAL-39] Refactor: Vote & Choice 모델 필드 수정 및 마이그레이션

--- vote/models.py
+++ vote/models.py
@@ -4,10 +4,11 @@ from account.models import TimeStampedModel, User
 
 
 class Vote(TimeStampedModel):
+    # Enum class : IntegerChoices VS. TextChoices
     class StateOfVote(models.IntegerChoices):
-        ONGOING = (0, '진행 중인 투표')
-        FINISHED = (1, '완료된 투표')
-        TRACKED = (2, '트래킹된 투표')
+        ONGOING = (1, '진행 중인 투표')
+        FINISHED = (2, '완료된 투표')
+        TRACKED = (3, '트래킹된 투표')
 
     class DurationOfQuestion(models.TextChoices):
         DAY = ('day', '1일 후')
@@ -18,17 +19,8 @@ class Vote(TimeStampedModel):
         UP = ('up', '올라갈까요')
         DOWN = ('down', '내려갈까요')
 
-    vote_id = models.IntegerField('투표 ID 번호', primary_key=True)
-    """
-    아직 코인 서버 연결 X
-    coin = models.ForeignKey(
-        'Coin', 
-        on_delete=models.CASCADE, 
-        verbose_name='코인의 코드',
-        related_name='',
-        related_query_name=''
-    )
-    """
+    # 다른 모델과의 관계
+    vote_id = models.BigAutoField('투표 ID 번호', primary_key=True)
     uploader = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -43,10 +35,20 @@ class Vote(TimeStampedModel):
         related_query_name='participated_vote',
         through='Choice'
     )
+    """
+    아직 코인 서버 연결 X
+    coin = models.ForeignKey(
+        'Coin', 
+        on_delete=models.CASCADE, 
+        verbose_name='코인의 코드',
+        related_name='',
+        related_query_name=''
+    )
+    """
 
     # 투표 상태 정보
     state = models.IntegerField('투표 상태', choices=StateOfVote.choices, default=StateOfVote.ONGOING)
-    finished_at = models.DateTimeField('투표가 종료되는 시점')  # DateTimeField VS. DurationField
+    finished_at = models.DateTimeField('투표가 종료되는 시점')
     tracked_at = models.DateTimeField('투표가 트래킹되는 시점')
     created_price = models.IntegerField('투표 생성 시점의 코인 가격')
     finished_price = models.IntegerField('투표 종료 시점의 코인 가격', null=True)
@@ -60,14 +62,12 @@ class Vote(TimeStampedModel):
         '예상 기간',
         max_length=10,
         choices=DurationOfQuestion.choices,
-        default=DurationOfQuestion.DAY
     )
     range = models.IntegerField('예상 변동폭')
     comment = models.CharField(
         '예상 추이',
         max_length=10,
         choices=CommentOfQuestion.choices,
-        default=CommentOfQuestion.UP
     )
 
     # 유저들의 투표 현황
@@ -76,20 +76,14 @@ class Vote(TimeStampedModel):
     pos_whales = models.IntegerField('\'예\'를 선택한 웨일 수', default=0)
     neg_whales = models.IntegerField('\'아니오\'를 선택한 웨일 수', default=0)
 
-    def save(self, *args, **kwargs):
-        # 운영자 투표인 경우 지급/차감 고래밥 수 조정
-        if self.uploader.is_staff:
-            spent_point = 0
-            earned_point = 30
-            is_admin_vote = True
-            super().save(spent_point, earned_point, is_admin_vote, *args, **kwargs)
-        else:
-            super().save(*args, **kwargs)
-
 
 class Choice(TimeStampedModel):
-    choice_id = models.IntegerField('투표 행위 ID', primary_key=True)
+    class CaseOfChoice(models.IntegerChoices):
+        YES = 1
+        NO = 2
+
+    choice_id = models.BigAutoField('투표 행위 ID', primary_key=True)
     vote = models.ForeignKey(Vote, on_delete=models.CASCADE)
     participant = models.ForeignKey(User, on_delete=models.CASCADE)
-    choice = models.BooleanField('유저의 선택')  # BooleanField VS. IntegerField(choices)
+    choice = models.IntegerField('유저의 선택', choices=CaseOfChoice.choices)
     is_answer = models.BooleanField('유저의 정답 여부', null=True)
