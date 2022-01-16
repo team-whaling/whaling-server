ecf87a74bb99a0c70586e16bdce6cd3b167facc9
Min A <min2010011@gmail.com>
2022-01-17 01:23:36 +0900

    [WHAL-39] Feat: Choice Serializer에 투표 생성자의 투표 참여 제한을 위한 validator 추가

--- vote/serializers.py
+++ vote/serializers.py
@@ -17,9 +17,16 @@ def get_current_time():
 
 
 class ChoiceSerializer(serializers.ModelSerializer):
+    participant = serializers.HiddenField(label='투표에 참여한 유저', default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Choice
         fields = '__all__'
+
+    def validate(self, data):
+        if self.context['request'].user == data['vote'].uploader:
+            raise serializers.ValidationError({'participant': '투표 생성자는 투표에 참여할 수 없습니다.'})
+        return data
 
 
 class VoteSerializer(serializers.ModelSerializer):
