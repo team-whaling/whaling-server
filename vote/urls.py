ae4494623c4c467434fc47512a739cad482aecb0
Min A <min2010011@gmail.com>
2022-01-17 01:01:21 +0900

    [WHAL-39] Test: Vote & Choice 뷰셋 url router 추가

---
+++ vote/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import *
+
+vote_router = DefaultRouter()
+vote_router.register(r'votes', VoteViewSet)
+
+choice_router = DefaultRouter()
+choice_router.register(r'choices', ChoiceViewSet)
+
+urlpatterns = [
+    path('', include(vote_router.urls)),
+    path('', include(choice_router.urls))
+]
