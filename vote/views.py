d4babc2512f48acc8592d075eb0dde0cbeb06657
Min A <min2010011@gmail.com>
2022-01-17 01:00:52 +0900

    [WHAL-39] Test: Vote & Choice 뷰셋 생성

--- vote/views.py
+++ vote/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import *
+
+
+class VoteViewSet(viewsets.ModelViewSet):
+    serializer_class = VoteSerializer
+    queryset = Vote.objects.all()
+
+
+class ChoiceViewSet(viewsets.ModelViewSet):
+    serializer_class = ChoiceSerializer
+    queryset = Choice.objects.all()
