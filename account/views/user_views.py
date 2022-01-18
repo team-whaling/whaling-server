887d69fc7dfb6bf747fc4bd792806f42cebd22ed
Min A <min2010011@gmail.com>
2022-01-19 03:14:43 +0900

    [WHAL-58] Feat: User 닉네임 변경 뷰 추가

--- account/views/user_views.py
+++ account/views/user_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from account.serializers import UserSerializer
 
@@ -11,3 +12,10 @@ class UserViewSet(viewsets.ViewSet):
     def list(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+    @action(methods=['patch'], detail=False)
+    def nickname(self, request):
+        serializer = UserSerializer(request.user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_200_OK)
