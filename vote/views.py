15d17e90a6b6bd7fcdbed404aaf315e7ea61d458
Min A <min2010011@gmail.com>
2022-01-18 20:19:55 +0900

    [WHAL-58] Feat: Vote create view 응답 수정

--- vote/views.py
+++ vote/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 from .serializers import coin_serializers, vote_serializers, choice_serializers
-from .models import Vote, Choice
+from .models import Vote, Choice, Coin
 
 
-class VoteViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+class VoteViewSet(viewsets.GenericViewSet):
     queryset = Vote.objects.all()
-    serializer_class = vote_serializers.VoteCreateSerializer
+    serializer_class = vote_serializers.VoteListSerializer
 
     def list(self, request):
         queryset = self.get_queryset()
-        serializer = vote_serializers.VoteListSerializer(queryset, many=True)
+        serializer = self.get_serializer(queryset, many=True)
         for data in serializer.data:
             participants = data.pop('participants')
             if request.user.user_id in participants:
@@ -27,6 +27,15 @@ class VoteViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
         vote = get_object_or_404(queryset, pk=pk)
         serializer = vote_serializers.VoteDetailSerializer(vote, context={'user': request.user})
         return Response(serializer.data)
+
+    def create(self, request):
+        serializer = vote_serializers.VoteCreateSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        vote = serializer.save()
+        response = {
+            'vote_id': vote.vote_id
+        }
+        return Response(response)
 
 
 # class VoteModelViewSet(viewsets.ModelViewSet):
