1d746fe094d054d3a824f35bf18ea1fe201528c4
Hwanu Jung <sossont1@gmail.com>
2022-01-19 23:25:18 +0900

    [WHAL-58] Chore : Admin Page 간단히 수정

--- vote/admin.py
+++ vote/admin.py
@@ -5,14 +5,19 @@ from vote import models
 
 @admin.register(models.Vote)
 class VoteAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['vote_id', 'coin', 'state', 'duration', 'range', 'comment', 'created_price', 'finished_price',
+                    'finished_at',
+                    'tracked_at', 'is_answer']
+    list_display_links = ['vote_id', 'state', 'created_price', 'finished_price', 'finished_at', 'tracked_at']
 
 
 @admin.register(models.Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['participant', 'choice', 'is_answer']
+    list_display_links = ['choice']
 
 
 @admin.register(models.Coin)
-class ChoiceAdmin(admin.ModelAdmin):
-    pass
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ['code', 'krname', 'name', 'image']
+    list_display_links = ['code', 'krname', 'name', 'image']
