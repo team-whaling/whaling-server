ddb9817c91bdad693baf10198764252b5ef22601
Min A <min2010011@gmail.com>
2022-01-25 00:48:12 +0900

    [WHAL-58] Feat: 카카오 토큰 발급 에러 예외 처리

--- account/views/auth_views.py
+++ account/views/auth_views.py
@@ -77,6 +77,8 @@ def kakao_login(request):
     # 카카오로부터 액세스 토큰 받기
     kakao_token_response = requests.post(kakao_token_url, headers=headers, data=data).json()
     access_token = kakao_token_response.get('access_token')
+    if access_token is None:
+        return Response(kakao_token_response, status=status.HTTP_400_BAD_REQUEST)
 
     # 사용자 정보 접근용 Request
     kakao_api_url = 'https://kapi.kakao.com/v2/user/me'
@@ -87,8 +89,6 @@ def kakao_login(request):
     # 카카오로부터 사용자 정보 받기
     kakao_api_response = requests.post(kakao_api_url, headers=headers).json()
     user_id = kakao_api_response.get('id')
-
-    # 카카오 API 응답에 에러가 있다면 에러 리턴
     if user_id is None:
         return Response(kakao_api_response, status=status.HTTP_400_BAD_REQUEST)
 
